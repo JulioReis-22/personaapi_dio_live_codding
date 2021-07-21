1803e3dc536661ddc180df4c93c83f9cdaa83a51
JulioReis-22 <julio.cesarreis@hotmail.com>
2021-07-20 22:39:42 -0300

    fazendo merda

--- src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
+++ src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
@@ -3,7 +3,7 @@ package one.digitalinnovation.personapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "one.digitalinnovation.personapi.controller")
 public class PersonapiApplication {
 
 	public static void main(String[] args) {
